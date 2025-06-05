53caef79c35c3f6849b27daf5f8fe4720de916a0
theo-mrn <theo.morin1402@gmail.com>
2025-06-05 21:40:09 +0200

    V1.3

--- src/app/images/varenna/photos.tsx
+++ src/app/images/varenna/photos.tsx
@@ -12,7 +12,7 @@ export function AppleCardsCarouselDemo() {
   return (
     <div className="w-full h-full py-20">
       <h2 className="max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200 font-sans">
-        Get to know your iSad.
+        Autres photos
       </h2>
       <Carousel items={cards} />
     </div>
